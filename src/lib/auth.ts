294e5ebe887cbb509e223af6dec50f47dce81d7e
Baseer Afridi <githubbs7@gmail.com>
2025-10-26 07:27:37 -0400

    feat(auth): add email verification and send verification emails on sign up and sign in
    - Updated auth configuration to require email verification.
    - Implemented email verification functionality with custom email sending logic.
    - Added new email sending utility using nodemailer.
    - Removed outdated ProtonVPN package from the project.
    - Updated pnpm workspace to include new dependencies: sharp and unrs-resolver.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { nextCookies } from "better-auth/next-js";
 import { getClient } from "./dbconnection";
+import { sendEmail } from "./email";
 
 
 export const auth = betterAuth({
@@ -10,11 +11,33 @@ export const auth = betterAuth({
 
     emailAndPassword: {
         enabled: true,
+        requireEmailVerification: true,
     },
+    emailVerification: {
+        sendOnSignUp: true,
+        sendOnSignIn: true,
+        autoSignInAfterVerification: true,
+        sendVerificationEmail: async ({ user, url, token }, request) => {
+            try {
+                await sendEmail({
+                    to: user.email,
+                    subject: 'Verify your email address',
+                    text: `Click the link to verify your email: ${url}`,
+                });
+                console.log(`Verification email sent to ${user.email}`);
+            } catch (error) {
+                console.error(`Failed to send verification email to ${user.email}:`, error);
+                throw error; // Propagate error to Better Auth
+            }
+
+        }
+    },
+
+
     socialProviders: {
         google: {
             enabled: true,
-            prompt: "select_account", 
+            prompt: "select_account",
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
             allowEmailLogin: true,
